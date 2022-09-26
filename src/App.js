c5ce19316161014c461b91ab8c7d5b4ab53f7c1b
Levi <megaraidsserver@gmail.com>
2022-09-26 16:11:11 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import './App.css';
-import Navbar from './components/navbar/Navbar';
-import Hero from './components/hero/Hero'
-import About from './components/about/About';
-import Developer from './components/developer/Developer';
-import Subscribe from './components/subscribe/Subscribe';
-import Footer from './components/footer/Footer';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import Hero from "./components/hero/Hero";
+import About from "./components/about/About";
+import Developer from "./components/developer/Developer";
+import Subscribe from "./components/subscribe/Subscribe";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
-      <About />
+      {/* <About />
       <Developer />
       <Subscribe />
-      <Footer />
+      <Footer /> */}
     </div>
   );
 }
